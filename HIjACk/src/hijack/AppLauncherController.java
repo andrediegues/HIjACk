357f5ed0416d214f1d42377e5fcc2e41f93273fe
andrediegues <andrediegues@github.com>
2018-05-23 15:31:31 +0100

    Changed the way the controller accesses the current stage.

--- HIjACk/src/hijack/AppLauncherController.java
+++ HIjACk/src/hijack/AppLauncherController.java
@@ -3,7 +3,6 @@ package hijack;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,9 +12,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -23,26 +23,20 @@ import javafx.stage.Stage;
 public class AppLauncherController {
     
     @FXML
-    private VBox vbox;
-    
-    @FXML
     void handleExitAction(ActionEvent event) {
-        Stage stage = (Stage) vbox.getScene().getWindow();
+        Stage stage = HIjACk.getCurrentStage();
         stage.close();
     }
 
     @FXML
     void handleHelpAction(ActionEvent event) {
-        Alert about = new Alert(Alert.AlertType.NONE);
-        about.setTitle("About HIjACK");
-        about.getButtonTypes().add(ButtonType.OK);
-        about.setGraphic(new Hyperlink("https://github.com/andrediegues/HIjACk"));        
+        Alert about = HIjACk.createAlert(Alert.AlertType.NONE, "About HIjACK", "", new Hyperlink("https://github.com/andrediegues/HIjACk"), new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE));
         about.showAndWait();
     }
     
     @FXML
     void handleLoadFolderAction(ActionEvent event) {
-        Stage s = (Stage) vbox.getScene().getWindow();
+        Stage s = HIjACk.getCurrentStage();
         getFolderFromStage(s);
     }
 
@@ -50,22 +44,21 @@ public class AppLauncherController {
         DirectoryChooser dc = new DirectoryChooser();
         dc.setInitialDirectory(new File(System.getProperty("user.home")));
         File choice = dc.showDialog(s);
-        if(choice == null){
-        }
-        else{
+        if(choice != null){
             try {                
-                FXMLLoader app                      = new FXMLLoader(getClass().getResource("application.fxml"));
-                Parent appRoot                      = app.load();
+                FXMLLoader app = new FXMLLoader(getClass().getResource("application.fxml"));
+                Parent appRoot = app.load();
                 ApplicationController appController = app.getController();
                 
                 List<String> listOfNames = new ArrayList<>();
-                for(File f: choice.listFiles((File dir, String name) -> (name.toLowerCase().contains(".jp") || name.toLowerCase().contains(".png") || name.toLowerCase().endsWith("if")))){
+                for(File f: choice.listFiles((File dir, String name) -> name.toLowerCase().contains(".jp") || name.toLowerCase().contains(".png") 
+                        || name.toLowerCase().endsWith("if"))){
                     listOfNames.add(f.getName());
                 }     
                 if(listOfNames.isEmpty()){
-                    Alert emptyDirectory = new Alert(Alert.AlertType.NONE, "Didn't find images. Please choose a different folder.", ButtonType.OK);
-                    emptyDirectory.setTitle("Error");
-                    emptyDirectory.showAndWait();
+                    Alert emptyFolder = HIjACk.createAlert(Alert.AlertType.NONE, "Found 0 images!", "Didn't find images. Please choose a different folder.", 
+                            new ImageView("images/ic_error_black_48dp.png"), new ButtonType("OK", ButtonBar.ButtonData.OK_DONE));
+                    emptyFolder.showAndWait();
                 }
                 else{
                     s.close();
@@ -76,12 +69,12 @@ public class AppLauncherController {
                         }
                         return c;
                     });
-                    System.out.println("Loading folder " + choice);
                     Stage application = new Stage();
-                    appController.initData(application, choice, listOfNames);
                     application.setScene(new Scene(appRoot));
                     application.setTitle("HIjACk");
+                    HIjACk.setCurrentStage(application);
                     application.show();
+                    appController.initData(choice, listOfNames);
                 }
             } catch (IOException ex) {
                 Logger.getLogger(AppLauncherController.class.getName()).log(Level.SEVERE, null, ex);
